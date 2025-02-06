b90764a0555f39f54f379d5f071e0a411529a7f8
komal <singh777komal@gmail.com>
2025-02-06 19:10:27 +0530

    added spotlight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div>
       <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] text-white">
-        <h1 className="text-2xl text-center">Chai aur code</h1>
       <Herosection/>
       </main>
     </div>
