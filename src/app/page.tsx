2e4a2a2a118deb46f85d29fcebd17589e0d09ee3
komal <singh777komal@gmail.com>
2025-02-09 19:41:35 +0530

    sticky-scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Herosection from '@/components/Herosection'
 import FeaturesCourses from "@/components/FeaturesCourses";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] text-white">
       <Herosection/>
       <FeaturesCourses/>
+      <WhyChooseUs/>
       </main>
     </div>
   );
